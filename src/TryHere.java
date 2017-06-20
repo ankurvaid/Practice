0d5231cff5ed88195495df45f24c7d813d1c6a4f
Ankur Vaid <vaid.ankur@tftus.com>
2017-06-20 17:04:54 +0530

    Created Odd Even mehtod to find if a number is Odd or Even

---
+++ src/TryHere.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Created by vaid.ankur on 5/18/2017.
+ */
+public class TryHere
+{
+    protected static void oddEven(int userInput)
+    {
+            if(userInput % 2 == 0)
+            {
+                System.out.println("The number that you entered is Even...!!");
+            }
+            else
+            {
+                System.out.println("The number that you entered is Odd...!!");
+            }
+    }
+
+
+
+
+
+    public static void main(String args[])
+    {
+        oddEven(0);
+
+    }
+}
